f30a777564ecf67933aa9c49788d59bb96cf5bed
abedinalways <nion.sk5067@gmail.com>
2026-09-08 11:46:27 +0600

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://10.10.9.51:5000/api/:path*",
-      },
-    ];
-  },
-  allowedDevOrigins: ["http://10.10.9.45:3000"],
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "http://10.10.9.51:5000/api/:path*",
+  //     },
+  //   ];
+  // },
+  // allowedDevOrigins: ["http://10.10.9.45:3000"],
   images: {
     remotePatterns: [
       {
